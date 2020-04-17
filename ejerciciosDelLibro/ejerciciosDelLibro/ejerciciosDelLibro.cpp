6b5e3f9e555287421caba10f5e4af8172938195f
ItaloKanchaVilca <italo.kancha@ucsp.edu.pe>
2020-04-17 09:16:32 -0500

    2.16 añadido

--- ejerciciosDelLibro/ejerciciosDelLibro/ejerciciosDelLibro.cpp
+++ ejerciciosDelLibro/ejerciciosDelLibro/ejerciciosDelLibro.cpp
@@ -3,14 +3,35 @@
 #include <iostream>
 using namespace std;
 
-int main()
+//ejercicio 2.16
+/**
 {
+	int num1, num2, suma, resta, multiplicacion, division, modulo;
 
+	cout << "inserte un numero 1: " << endl;
+	cin >> num1;
 
+	cout << "inserte un numero 2: " << endl;
+	cin >> num2;
 
+	cout << "el numero 1 es: " << num1 << endl;
+	cout << "el numero 2 es: " << num2 << endl;
 
+	suma = num1 + num2;
+	resta = num1 - num2;
+	multiplicacion = num1 * num2;
+	division = num1 / num2;
+	modulo = num1 % num2;
 
+	cout << "la suma de los numeros son : " << suma << endl;
+	cout << "la resta del numero1 y numero2 : " << resta << endl;
+	cout << "la multiplicacion de numero1 y numero2 : " << multiplicacion << endl;
+	cout << "la division de numero1 y numero2 es : " << division << endl;
+	cout << "el redyo de numero1 y numero2 es : " << modulo << endl;
 
 	return 0;
 }
+*/
+
+//ejercicio 2.19
 
